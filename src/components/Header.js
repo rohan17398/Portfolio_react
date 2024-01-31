c609db3b68546468a6fcec8dd05929071ff4a0ea
Rohan Baldha <baldharohan@gmail.com>
2024-01-31 13:55:46 -0800

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,14 @@ const Header = () => {
   return (
     <nav className="space-x-10">
       <ul className="flex flex-row space-x-4 md:space-x-10 md:text-xl justify-center md:justify-end font-medium">
+      <li>
+          <a
+            href="#experience"
+            className="py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg"
+          >
+            Expereince
+          </a>
+        </li>
         <li>
           <a
             href="#projects"
