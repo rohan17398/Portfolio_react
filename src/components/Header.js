ac9517938b301c071754c96101b9e7b42339ffa1
Rohan Baldha <baldharohan@gmail.com>
2024-02-07 11:08:01 -0800

    Spelling change

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             href="#experience"
             className="py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg"
           >
-            Expereince
+            Experience
           </a>
         </li>
         <li>
